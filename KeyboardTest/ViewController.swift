2264ae38d4595f6c967e1781d1e3877ce3983dc6
tsabend <tsabend@gmail.com>
2015-11-20 07:40:19 -0500

    Sections

--- KeyboardTest/ViewController.swift
+++ KeyboardTest/ViewController.swift
@@ -18,14 +18,15 @@ class ViewController: UIViewController {
         self.view.backgroundColor = UIColor.greenColor()
         
         self.addChildViewController(keyboard)
-        let halfY = self.view.bounds.height / 2
+        let quartY = self.view.bounds.height / 4
         self.view.addSubview(keyboard.view)
-        keyboard.view.frame = CGRect(origin: CGPoint(x: 0, y: halfY), size: CGSize(width: self.view.frame.width, height: halfY))
+        keyboard.view.frame = CGRect(origin: CGPoint(x: 0, y: self.view.bounds.height - quartY), size: CGSize(width: self.view.frame.width, height: quartY))
         keyboard.didMoveToParentViewController(self)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        
     }
 }
 
@@ -38,8 +39,8 @@ class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayo
         super.viewDidLoad()
 
         
-        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
-        layout.sectionInset = UIEdgeInsets(top: 5, left: 20, bottom: 20, right: 20)
+        let layout: UICollectionViewFlowLayout = KeyboardCollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 20, bottom: 20, right: 20)
         layout.itemSize = CGSize(width: 40, height: 40)
         layout.scrollDirection = .Horizontal
 
@@ -56,7 +57,11 @@ class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayo
     }
 
     func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4000
+        return 10
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 6
     }
     
     func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
@@ -65,31 +70,68 @@ class KeyboardViewController: UIViewController, UICollectionViewDelegateFlowLayo
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as! KeyboardCollectionViewCell
-        cell.backgroundColor = UIColor.orangeColor()
+
+        switch indexPath.section {
+        case 0:
+            cell.backgroundColor = UIColor.orangeColor()
+        case 1:
+            cell.backgroundColor = UIColor.blueColor()
+        case 2:
+            cell.backgroundColor = UIColor.blackColor()
+        case 3:
+            cell.backgroundColor = UIColor.purpleColor()
+        case 4:
+            cell.backgroundColor = UIColor.greenColor()
+        default:
+            cell.backgroundColor = UIColor.grayColor()
+        }
         cell.label.text = indexPath.row.description
         return cell
-    }    
+    }
+    
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-     
+        self.collectionView.frame = self.view.bounds
     }
 
 
 }
 
+
+class KeyboardCollectionViewFlowLayout: UICollectionViewFlowLayout {
+    
+    override func collectionViewContentSize() -> CGSize {
+
+        return CGSize(width: 5000, height: 20)
+    }
+
+}
+
+
 class KeyboardCollectionViewCell: UICollectionViewCell {
     
     let label = UILabel()
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        self.addSubview(label)
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        label.font = UIFont.boldSystemFontOfSize(20)
+        label.textColor = UIColor.blueColor()
+        self.contentView.addSubview(label)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
         self.label.sizeToFit()
+        self.label.frame.origin = self.contentView.center
     }
+    
+    
 }
